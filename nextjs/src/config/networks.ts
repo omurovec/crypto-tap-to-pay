56e135567972e14c69fdd60b4adab63b1acc06e6
omurovec <owenmurovec@gmail.com>
2024-11-17 02:40:16 +0700

    add token addresses

--- nextjs/src/config/networks.ts
+++ nextjs/src/config/networks.ts
@@ -18,6 +18,11 @@ export const DEPLOYMENT_ADDRESSES: { [key in NetworkType]: `0x${string}` } = {
   84532: "0x705F3124762253c8eF6b7a39f6C0AB9a6c2961FF",
 };
 
+export const TOKEN_ADDRESSES: { [key in NetworkType]: `0x${string}` } = {
+  8453: "0x0000000000000000000000000000000000000000",
+  84532: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+};
+
 export const COINBASE_API_NETWORK_CONFIG: { [key in NetworkType]: string } = {
   8453: "base-mainnet",
   84532: "base-sepolia",
