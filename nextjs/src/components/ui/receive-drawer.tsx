2b402bd55280a04aa477cef3c271516296bf4608
omurovec <owenmurovec@gmail.com>
2024-11-16 23:18:04 +0700

    update UI

--- nextjs/src/components/ui/receive-drawer.tsx
+++ nextjs/src/components/ui/receive-drawer.tsx
@@ -76,7 +76,7 @@ export default function ReceiveDrawer() {
     <div className="flex-1">
       <Drawer className="flex-1">
         <DrawerTrigger className="w-full">
-          <Button className="w-full">Receive</Button>
+          <Button className="w-full h-14 bg-slate-800">Receive</Button>
         </DrawerTrigger>
         <DrawerContent>
           {received ? (
