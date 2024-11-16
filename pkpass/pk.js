7380a68223dae981956b54ac30b92ceb5e44be7e
Simone D'Amico <sim@me.com>
2024-11-16 16:15:03 +0700

    removed old ios project and added pkpass script

---
+++ pkpass/pk.js
@@ -0,0 +1,23 @@
+import { createPass } from "passkit-generator";
+
+try {
+	const examplePass = await createPass({
+		model: "path/to/exported/model/Untitled Project.pkpass",
+		certificates: {
+			wwdr: "./certs/wwdr.pem",
+			signerCert: "./certs/signercert.pem",
+			signerKey: {
+				keyFile: "./certs/signerkey.pem",
+				passphrase: "123456"
+			}
+		},
+	});
+
+
+	// Generate the stream, which gets returned through a Promise
+	const stream: Stream = examplePass.generate();
+
+	doSomethingWithTheStream(stream);
+} catch (err) {
+	doSomethingWithTheError(err);
+}
