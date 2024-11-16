75c5defff73c03434e5d7d34202f9107f390f08a
keinberger <philipp.keinberger@gmail.com>
2024-11-16 22:25:26 +0700

    feat: add templates for useTransactionHistory & useCheckSignature

---
+++ nextjs/src/hooks/useTransactionHistory.ts
@@ -0,0 +1,5 @@
+// TODO: should return an array of transactions for a given wallet address (either payments made or received)
+// add status for outgoing transaction if it is pending or not
+// add status for received payments if they have been claimed or not
+
+// TODO: do this using subgraph
